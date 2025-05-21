56af38dfbabf286e781a2284f8af679a8765d94c
Aran Kieskamp <aranna00@gmail.com>
2025-05-21 19:50:43 +0200

    Started Frieslandhuurt scraper

---
+++ HestiaSharp/FrieslandHuurt/FrieslandHuurtScraper.cs
@@ -0,0 +1,13 @@
+﻿namespace HestiaSharp.FrieslandHuurt
+{
+    public class FrieslandHuurtScraper(DateTime lastScrape) : IScraper
+    {
+        private string _apiUrl
+            = "https://frieslandhuurt-aanbodapi.zig365.nl/api/v1/actueel-aanbod?locale=nl_NL&sort=+availableFromDate";
+
+        private DateTime _lastScrape = lastScrape;
+        private string filterSinceLastScrape = "";
+
+        public void SetBaseFilter() { }
+    }
+}
---
+++ HestiaSharp/FrieslandHuurt/Frieslandhuurt.cs
@@ -0,0 +1,11 @@
+﻿namespace HestiaSharp.FrieslandHuurt
+{
+    public class Frieslandhuurt : IAgency
+    {
+        public IScraper Scraper { get; set; }
+
+        public string Name { get; set; } = "Frieslandhuurt";
+
+        public string BaseUrl { get; set; }
+    }
+}
